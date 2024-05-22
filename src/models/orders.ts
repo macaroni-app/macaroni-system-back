62fddd8be122b1dd16d6f21d25dd6a22942d790d
Marcos Imfeld <marcosimfeld34@gmail.com>
2024-05-22 14:49:13 -0300

    commented all order

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -1,105 +1,105 @@
-import { model, Schema, Document, SchemaTypes } from 'mongoose'
+// import { model, Schema, Document, SchemaTypes } from 'mongoose'
 
-export enum OrderStatus {
-  NEW = 'NEW',
-  PREPARED = 'PREPARED',
-  DELIVERED = 'DELIVERED'
-}
+// export enum OrderStatus {
+//   NEW = 'NEW',
+//   PREPARED = 'PREPARED',
+//   DELIVERED = 'DELIVERED'
+// }
 
-export interface IOrder extends Document {
-  isRetail: boolean
-  client: string
-  total: number
-  status: OrderStatus
-  isDeleted: boolean
-  createdAt: Date
-  updatedAt: Date
-  sortingDate: Date
-  deletedAt: Date
-  createdBy: string
-  updatedBy: string
-}
+// export interface IOrder extends Document {
+//   isRetail: boolean
+//   client: string
+//   total: number
+//   status: OrderStatus
+//   isDeleted: boolean
+//   createdAt: Date
+//   updatedAt: Date
+//   sortingDate: Date
+//   deletedAt: Date
+//   createdBy: string
+//   updatedBy: string
+// }
 
-const orderSchema = new Schema({
-  isRetail: {
-    type: Boolean,
-    default: false
-  },
-  client: {
-    type: SchemaTypes.ObjectId,
-    ref: 'client',
-    required: true
-  },
-  total: {
-    type: Number,
-    required: true
-  },
-  status: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    inmutable: true,
-    default: () => {
-      const now = new Date().toLocaleString('es-MX', {
-        timeZone: 'America/Argentina/Buenos_Aires'
-      })
-      const dateWithoutTime = now.split(',')[0]
+// const orderSchema = new Schema({
+//   isRetail: {
+//     type: Boolean,
+//     default: false
+//   },
+//   client: {
+//     type: SchemaTypes.ObjectId,
+//     ref: 'client',
+//     required: true
+//   },
+//   total: {
+//     type: Number,
+//     required: true
+//   },
+//   status: {
+//     type: String,
+//     required: true
+//   },
+//   createdAt: {
+//     type: Date,
+//     inmutable: true,
+//     default: () => {
+//       const now = new Date().toLocaleString('es-MX', {
+//         timeZone: 'America/Argentina/Buenos_Aires'
+//       })
+//       const dateWithoutTime = now.split(',')[0]
 
-      const day = dateWithoutTime.split('/')[0]
-      const month = dateWithoutTime.split('/')[1]
-      const year = dateWithoutTime.split('/')[2]
+//       const day = dateWithoutTime.split('/')[0]
+//       const month = dateWithoutTime.split('/')[1]
+//       const year = dateWithoutTime.split('/')[2]
 
-      const dateToSave = new Date(`${month}/${day}/${year}`)
-      dateToSave.setHours(12, 0, 0, 0)
+//       const dateToSave = new Date(`${month}/${day}/${year}`)
+//       dateToSave.setHours(12, 0, 0, 0)
 
-      return dateToSave
-    }
-  },
-  updatedAt: {
-    type: Date,
-    default: () => {
-      const now = new Date().toLocaleString('es-MX', {
-        timeZone: 'America/Argentina/Buenos_Aires'
-      })
-      const dateWithoutTime = now.split(',')[0]
+//       return dateToSave
+//     }
+//   },
+//   updatedAt: {
+//     type: Date,
+//     default: () => {
+//       const now = new Date().toLocaleString('es-MX', {
+//         timeZone: 'America/Argentina/Buenos_Aires'
+//       })
+//       const dateWithoutTime = now.split(',')[0]
 
-      const day = dateWithoutTime.split('/')[0]
-      const month = dateWithoutTime.split('/')[1]
-      const year = dateWithoutTime.split('/')[2]
+//       const day = dateWithoutTime.split('/')[0]
+//       const month = dateWithoutTime.split('/')[1]
+//       const year = dateWithoutTime.split('/')[2]
 
-      const dateToSave = new Date(`${month}/${day}/${year}`)
+//       const dateToSave = new Date(`${month}/${day}/${year}`)
 
-      dateToSave.setHours(12, 0, 0, 0)
+//       dateToSave.setHours(12, 0, 0, 0)
 
-      return dateToSave
-    }
-  },
-  sortingDate: {
-    type: Date,
-    inmutable: true,
-    default: () => new Date()
-  },
-  deletedAt: {
-    type: Date
-  },
-  isDeleted: {
-    type: Boolean,
-    default: false
-  },
-  createdBy: {
-    type: SchemaTypes.ObjectId,
-    ref: 'user',
-    required: true
-  },
-  updatedBy: {
-    type: SchemaTypes.ObjectId,
-    ref: 'user',
-    required: true
-  }
-})
+//       return dateToSave
+//     }
+//   },
+//   sortingDate: {
+//     type: Date,
+//     inmutable: true,
+//     default: () => new Date()
+//   },
+//   deletedAt: {
+//     type: Date
+//   },
+//   isDeleted: {
+//     type: Boolean,
+//     default: false
+//   },
+//   createdBy: {
+//     type: SchemaTypes.ObjectId,
+//     ref: 'user',
+//     required: true
+//   },
+//   updatedBy: {
+//     type: SchemaTypes.ObjectId,
+//     ref: 'user',
+//     required: true
+//   }
+// })
 
-const Order = model<IOrder>('order', orderSchema)
+// const Order = model<IOrder>('order', orderSchema)
 
-export default Order
+// export default Order
