b258d34bc43c35b88578f317f69024241261fc20
Marcos Imfeld <marcosimfeld34@gmail.com>
2024-05-28 15:23:12 -0300

    edit permissions for users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -17,7 +17,7 @@ usersRouter.get('/refresh', usersController.refreshToken as RequestHandler)
 usersRouter.post('/register', [verifyToken as RequestHandler, verifyRole(ProfileBase.users.create)], usersController.store as RequestHandler)
 usersRouter.put('/soft-delete/:id', [verifyToken as RequestHandler, verifyRole(ProfileBase.users.deactivate)], usersController.changeIsActive as RequestHandler)
 usersRouter.put('/:id', [verifyToken as RequestHandler, verifyRole(ProfileBase.users.edit)], usersController.update as RequestHandler)
-usersRouter.put('/change-password/:id', [verifyToken as RequestHandler, verifyRole(ProfileBase.users.edit)], usersController.changePassword as RequestHandler)
+usersRouter.put('/change-password/:id', [verifyToken as RequestHandler, verifyRole(ProfileBase.users.editOwnInfo)], usersController.changePassword as RequestHandler)
 usersRouter.delete('/:id', [verifyToken as RequestHandler, verifyRole(ProfileBase.users.delete)], usersController.delete as RequestHandler)
 
 // usersRouter.put("/recovery-password", usersController.recoverPassword);
