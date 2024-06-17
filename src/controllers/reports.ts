f8507e6217d22fc2d66ea0a53bf5b9fc91a22095
Marcos Imfeld <marcosimfeld34@gmail.com>
2024-06-17 18:22:37 -0300

    .

--- src/controllers/reports.ts
+++ src/controllers/reports.ts
@@ -8,130 +8,130 @@ import { GetSaleQueryType, SaleType } from '../schemas/sales'
 import { GetSaleItemQueryType, SaleItemType } from '../schemas/saleItems'
 
 const reportsController = {
-	getAllSales: async (req: Request<{}, {}, {}, GetSaleQueryType>, res: Response): Promise<Response> => {
-		const { id } = req.query
+  getAllSales: async (req: Request<{}, {}, {}, GetSaleQueryType>, res: Response): Promise<Response> => {
+    const { id } = req.query
 
-		const all = req.query.all === 'true'
+    const all = req.query.all === 'true'
 
-		const historyMonthToRetrieve = req.query.historyMonthToRetrieve
+    const historyMonthToRetrieve = req.query.historyMonthToRetrieve
 
-		let startDate = new Date()
-		let endDate = new Date()
+    let startDate = new Date()
+    let endDate = new Date()
 
-		if (req.query.startDate !== undefined) {
-			startDate = new Date(req.query.startDate)
-		}
+    if (req.query.startDate !== undefined) {
+      startDate = new Date(req.query.startDate)
+    }
 
-		if (req.query.endDate !== undefined) {
-			endDate = new Date(req.query.endDate)
-		}
+    if (req.query.endDate !== undefined) {
+      endDate = new Date(req.query.endDate)
+    }
 
-		const firstDayOfMonth = new Date(startDate.getFullYear(), startDate.getMonth(), 1)
-		const lastDayOfMonth = new Date(endDate.getFullYear(), endDate.getMonth() + 1, 0)
+    const firstDayOfMonth = new Date(startDate.getFullYear(), startDate.getMonth(), 1)
+    const lastDayOfMonth = new Date(endDate.getFullYear(), endDate.getMonth() + 1, 0)
 
-		startDate = firstDayOfMonth
-		endDate = lastDayOfMonth
+    startDate = firstDayOfMonth
+    endDate = lastDayOfMonth
 
-		if (historyMonthToRetrieve !== undefined) {
-			startDate = new Date()
-			startDate.setDate(0)
-			startDate.setMonth(startDate.getMonth() + 1)
-			startDate.setFullYear(startDate.getFullYear() - 1)
+    if (historyMonthToRetrieve !== undefined) {
+      startDate = new Date()
+      startDate.setDate(0)
+      startDate.setMonth(startDate.getMonth() + 1)
+      startDate.setFullYear(startDate.getFullYear() - 1)
 
-			endDate = new Date()
-			endDate.setDate(endDate.getDate() + 1)
-		}
+      endDate = new Date()
+      endDate.setDate(endDate.getDate() + 1)
+    }
 
-		const filters = {
-			$expr: {
-				$and: [
-					{ $eq: ['$isDeleted', false] },
-					{ $gte: ['$createdAt', startDate] },
-					{ $lte: ['$createdAt', endDate] }
-				]
-			}
-		}
+    const filters = {
+      $expr: {
+        $and: [
+          { $eq: ['$isDeleted', false] },
+          { $gte: ['$createdAt', startDate] },
+          { $lte: ['$createdAt', endDate] }
+        ]
+      }
+    }
 
-		const sales: SaleType[] = await reportService.getAllSales(
-			(id === undefined || id === null)
-				? !all
-					? filters
-					: {}
-				: {
-					$expr: {
-						$and: [{ $eq: ['$_id', id] }]
-					}
-				}
-		)
+    const sales: SaleType[] = await reportService.getAllSales(
+      (id === undefined || id === null)
+        ? !all
+            ? filters
+            : {}
+        : {
+            $expr: {
+              $and: [{ $eq: ['$_id', id] }]
+            }
+          }
+    )
 
-		return res.status(200).json({
-			status: 200,
-			total: sales.length,
-			data: sales
-		})
-	},
-	getAllSaleItems: async (req: Request<{}, {}, {}, GetSaleItemQueryType>, res: Response): Promise<Response> => {
-		const { id } = req.query
+    return res.status(200).json({
+      status: 200,
+      total: sales.length,
+      data: sales
+    })
+  },
+  getAllSaleItems: async (req: Request<{}, {}, {}, GetSaleItemQueryType>, res: Response): Promise<Response> => {
+    const { id } = req.query
 
-		const all = req.query.all === 'true'
+    const all = req.query.all === 'true'
 
-		const historyMonthToRetrieve = req.query.historyMonthToRetrieve
+    const historyMonthToRetrieve = req.query.historyMonthToRetrieve
 
-		let startDate = new Date()
-		let endDate = new Date()
+    let startDate = new Date()
+    let endDate = new Date()
 
-		if (req.query.startDate !== undefined) {
-			startDate = new Date(req.query.startDate)
-		}
+    if (req.query.startDate !== undefined) {
+      startDate = new Date(req.query.startDate)
+    }
 
-		if (req.query.endDate !== undefined) {
-			endDate = new Date(req.query.endDate)
-		}
+    if (req.query.endDate !== undefined) {
+      endDate = new Date(req.query.endDate)
+    }
 
-		const firstDayOfMonth = new Date(startDate.getFullYear(), startDate.getMonth(), 1)
-		const lastDayOfMonth = new Date(endDate.getFullYear(), endDate.getMonth() + 1, 0)
+    const firstDayOfMonth = new Date(startDate.getFullYear(), startDate.getMonth(), 1)
+    const lastDayOfMonth = new Date(endDate.getFullYear(), endDate.getMonth() + 1, 0)
 
-		startDate = firstDayOfMonth
-		endDate = lastDayOfMonth
+    startDate = firstDayOfMonth
+    endDate = lastDayOfMonth
 
-		if (historyMonthToRetrieve !== undefined) {
-			startDate = new Date()
-			startDate.setDate(0)
-			startDate.setMonth(startDate.getMonth() + 1)
-			startDate.setFullYear(startDate.getFullYear() - 1)
+    if (historyMonthToRetrieve !== undefined) {
+      startDate = new Date()
+      startDate.setDate(0)
+      startDate.setMonth(startDate.getMonth() + 1)
+      startDate.setFullYear(startDate.getFullYear() - 1)
 
-			endDate = new Date()
-			endDate.setDate(endDate.getDate() + 1)
-		}
+      endDate = new Date()
+      endDate.setDate(endDate.getDate() + 1)
+    }
 
-		const filters = {
-			$expr: {
-				$and: [
-					{ $eq: ['$isDeleted', false] },
-					{ $gte: ['$createdAt', startDate] },
-					{ $lte: ['$createdAt', endDate] }
-				]
-			}
-		}
+    const filters = {
+      $expr: {
+        $and: [
+          { $eq: ['$isDeleted', false] },
+          { $gte: ['$createdAt', startDate] },
+          { $lte: ['$createdAt', endDate] }
+        ]
+      }
+    }
 
-		const sales: SaleItemType[] = await reportService.getAllSaleItems(
-			(id === undefined || id === null)
-				? !all
-					? filters
-					: {}
-				: {
-					$expr: {
-						$and: [{ $eq: ['$_id', id] }]
-					}
-				}
-		)
+    const sales: SaleItemType[] = await reportService.getAllSaleItems(
+      (id === undefined || id === null)
+        ? !all
+            ? filters
+            : {}
+        : {
+            $expr: {
+              $and: [{ $eq: ['$_id', id] }]
+            }
+          }
+    )
 
-		return res.status(200).json({
-			status: 200,
-			total: sales.length,
-			data: sales
-		})
-	}
+    return res.status(200).json({
+      status: 200,
+      total: sales.length,
+      data: sales
+    })
+  }
 }
 
 export default reportsController
