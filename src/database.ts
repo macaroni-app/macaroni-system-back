584d54d39bd4a332d519f65fda3df082b036d649
Marcos Imfeld <marcosimfeld34@gmail.com>
2024-07-29 22:39:48 -0300

    added env variable

--- src/database.ts
+++ src/database.ts
@@ -5,10 +5,11 @@ dotenv.config()
 const USER = process.env.USER ?? ''
 const PASSWORD = process.env.PASSWORD ?? ''
 const DBNAME = process.env.DBNAME ?? ''
+const CLUSTER_NAME = process.env.CLUSTER_NAME ?? ''
 
 // DB Connection
 mongoose.set('strictQuery', false)
-const uri = `mongodb+srv://${USER}:${PASSWORD}@cluster0.5pqkwyp.mongodb.net/${DBNAME}?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${USER}:${PASSWORD}@cluster0.${CLUSTER_NAME}.mongodb.net/${DBNAME}?retryWrites=true&w=majority`
 mongoose
   .connect(uri)
   .then(() => console.log('DB Connection OK'))
