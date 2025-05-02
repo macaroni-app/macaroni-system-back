dc77d3ce081aac7aec90bcc77cc29e2e6ea76745
Marcos Imfeld <marcosimfeld34@gmail.com>
2025-05-02 20:47:34 -0300

    trying to fix cors

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,21 +1,17 @@
 import cors from 'cors'
-// import allowedOrigins from '../config/allowedOrigins'
+import allowedOrigins from '../config/allowedOrigins'
 
 const corsOptions: cors.CorsOptions = {
-  // origin: (origin, callback) => {
-  //   if (allowedOrigins.includes(origin ?? '') || (origin == null)) {
-  //     callback(null, true)
-  //   } else {
-  //     callback(new Error('Not allowed by CORS'))
-  //   }
-  // },
-  // optionsSuccessStatus: 200,
-  // credentials: true,
-  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-  origin: 'https://macaroni-system-front.vercel.app', // especificá el dominio exacto
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin ?? '') || (origin == null)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  optionsSuccessStatus: 200
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }
 
 export default corsOptions
