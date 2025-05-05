b055d1690f9c8ba73794e3699e1ce528fcdf1f25
Marcos Imfeld <marcosimfeld34@gmail.com>
2025-05-05 19:59:15 -0300

    trying to fix cors

--- src/middlewares/credentials.ts
+++ src/middlewares/credentials.ts
@@ -2,17 +2,17 @@ import { Request, Response, NextFunction } from 'express'
 import allowedOrigins from '../config/allowedOrigins'
 
 const credentials = (req: Request, res: Response, next: NextFunction): void => {
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin ?? '')) {
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    next()
+  }
   // const origin = req.headers.origin
   // if (allowedOrigins.includes(origin ?? '')) {
   //   res.setHeader('Access-Control-Allow-Credentials', 'true')
-  //   next()
+  //   res.setHeader('Access-Control-Allow-Origin', origin) // 🔴 ¡esto es clave!
   // }
-  const origin = req.headers.origin
-  if (allowedOrigins.includes(origin ?? '')) {
-    res.header('Access-Control-Allow-Credentials', 'true')
-    res.header('Access-Control-Allow-Origin', origin) // 🔴 ¡esto es clave!
-  }
-  next()
+  // next()
 }
 
 export default credentials
