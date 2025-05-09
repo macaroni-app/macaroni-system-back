f74608587c5bb238c787c28f1775c6ef28bebc7e
Marcos Imfeld <marcosimfeld34@gmail.com>
2025-05-08 22:24:47 -0300

    fixing afipClient

--- src/afip/afipClient.ts
+++ src/afip/afipClient.ts
@@ -1,40 +1,34 @@
-import { parseStringPromise } from 'xml2js'
-import { afipConfig } from '../config/afipConfig'
 import * as soap from 'soap'
+import { parseStringPromise } from 'xml2js'
 import https from 'https'
+import { afipConfig } from '../config/afipConfig'
 
 export class AfipClient {
   private client: any
 
   async initClient (): Promise<void> {
-    if (!this.client) {
-      try {
-        const wsdlPath = afipConfig.wsfeUrl
+    if (this.client) return
 
-        const agent = new https.Agent({
-          minVersion: 'TLSv1',
-          secureOptions: require('constants').SSL_OP_LEGACY_SERVER_CONNECT,
-          rejectUnauthorized: false
-        })
+    try {
+      const agent = new https.Agent({
+        minVersion: 'TLSv1',
+        rejectUnauthorized: false,
+        secureOptions: require('constants').SSL_OP_LEGACY_SERVER_CONNECT
+      })
 
-        this.client = await soap.createClientAsync(wsdlPath, {
-          wsdl_options: { agent }
-        })
+      this.client = await soap.createClientAsync(afipConfig.wsfeUrl, {
+        wsdl_options: { agent }
+      })
 
-        console.log('Cliente SOAP inicializado correctamente')
-      } catch (error) {
-        console.error('Error al crear el cliente SOAP:', error)
-        throw new Error('No se pudo inicializar el cliente SOAP')
-      }
+      console.log('Cliente SOAP inicializado correctamente')
+    } catch (error) {
+      console.error('Error al crear el cliente SOAP:', error)
+      throw new Error('No se pudo inicializar el cliente SOAP')
     }
   }
 
   async callMethod (method: string, args: object): Promise<any> {
     await this.initClient()
-
-    if (typeof this.client[`${method}`] !== 'function') {
-      throw new Error(`El método '${method}' no está definido en el cliente SOAP.`)
-    }
 
     try {
       const response = await this.client[`${method}Async`](args)
@@ -42,14 +36,12 @@ export class AfipClient {
       if (Array.isArray(response) && response.length > 0) {
         const rawXml = response[1]
         try {
-          const parsed = await parseStringPromise(rawXml, { explicitArray: false })
-          return parsed
-        } catch (error) {
-          console.warn('No se pudo parsear XML, devolviendo JSON plano.')
+          return await parseStringPromise(rawXml, { explicitArray: false })
+        } catch {
           return response[0]
         }
       } else {
-        throw new Error('Respuesta SOAP inesperada.')
+        throw new Error('Respuesta SOAP inesperada')
       }
     } catch (error) {
       console.error('Error en la llamada SOAP:', error)
